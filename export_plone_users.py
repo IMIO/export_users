53ae9f8d528272d995ebd6a6cd9938b4eef5fc15
Benoit Suttor <bsuttor@imio.be>
2019-05-24 15:03:36 +0200

    Push users to memory

--- export_plone_users.py
+++ export_plone_users.py
@@ -9,98 +9,105 @@ from zope.globalrequest import setRequest
 import argparse
 import csv
 import logging
+import requests
 import sys
 
 
-logger = logging.getLogger('export_plone_users.py')
+logger = logging.getLogger("export_plone_users.py")
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s',
-                              '%Y-%m-%d %H:%M:%S')
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s %(name)s %(message)s", "%Y-%m-%d %H:%M:%S"
+)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-parser = argparse.ArgumentParser(description='Run a script')
-parser.add_argument('--plone-path', dest='plone_path',
-                    help='Set plone path (example: liege/liege)',
-                    default='')
-parser.add_argument('-c')  # use to bin/instance run script.py
+parser = argparse.ArgumentParser(description="Run a script")
+# parser.add_argument(
+#     "--plone-path",
+#     dest="plone_path",
+#     help="Set plone path (example: liege/liege)",
+#     default="",
+# )
+parser.add_argument("-c")  # use to bin/instance run script.py
+
+mun_id = "liege"
+app_id = "iA.Web"
+app_name = "users"
+memroy_base_url = "http://localhost:6543"
 
 
-def get_site(app, path=''):
-    zopeapp = makerequest.makerequest(app)
-    zopeapp.REQUEST['PARENTS'] = [app]
-    setRequest(zopeapp.REQUEST)
-    # newSecurityManager(None, user)
-    user = app.acl_users.getUser('admin')
-    newSecurityManager(None, user.__of__(app.acl_users))
-    portal = None
-    if not path:
-        for oid in app.objectIds():
-            obj = app[oid]
-            if IPloneSiteRoot.providedBy(obj):
-                portal = obj
-    else:
-        container = app
-        for name in path.split('/'):
-            if name in container.objectIds():
-                container = container[name]
-            else:
-                error = 'Bad path {0}'.format(name)
-                logger.error(error)
-                raise(error)
-        portal = container
-    if not portal:
-        error = 'Do not find portal'
-        logger.error(error)
-        raise(error)
-    setSite(portal)
-    return portal
-
-
-def get_users(portal):
+def get_users():
     users = []
     pas = api.portal.get().acl_users
     passwords = dict(pas.source_users._user_passwords)
     list_members = api.user.get_users()
     for member in list_members:
         user = {}
-        user['id'] = member.getId()
-        user['name'] = member.getProperty('fullname', member.getUserName())
-        user['email'] = member.getProperty('email', None)
-        # user['fullname'] = member.getProperty('fullname', None)
-        # user['roles'] = member.getRoles()
-        # user['domains'] = member.getDomains()
-        user['password'] = passwords.get(user['id'])
+        user["app_id"] = app_id
+        user["mun_id"] = mun_id
+        user["user_id"] = member.getId()
+        user["content_id"] = member.getId()
+        user["fullname"] = member.getProperty("fullname", member.getUserName())
+        user["email"] = member.getProperty("email", None)
+        user["password"] = passwords.get(user["user_id"])
         users.append(user)
     return users
 
 
-def create_csv(users, exported_filename='users.csv', delimiter=','):
-    with open(exported_filename, 'wb') as csvfile:
+def create_csv(users, exported_filename="users.csv", delimiter=","):
+    with open(exported_filename, "wb") as csvfile:
         filewriter = csv.writer(
-            csvfile,
-            delimiter=delimiter,
-            quotechar='|',
-            quoting=csv.QUOTE_MINIMAL
+            csvfile, delimiter=delimiter, quotechar="|", quoting=csv.QUOTE_MINIMAL
         )
         # first_line = users[0].keys()
-        first_line = ['id', 'name', 'email', 'password']
+        first_line = ["id", "name", "email", "password"]
         filewriter.writerow(first_line)
         for user in users:
             user_line = [user[id] for id in first_line]
             filewriter.writerow(user_line)
-        logger.info(
-            '{0} users exported on {1}'.format(len(users), exported_filename))
+        logger.info("{0} users exported on {1}".format(len(users), exported_filename))
 
 
-if __name__ == '__main__':
+def add_municipality(mun_id):
+    url = "{0}/{1}".format(memroy_base_url, app_name)
+    req = requests.get(url)
+    if mun_id not in req.json():
+        params = {"container_id": mun_id}
+        req = requests.post(url, json=params)
+
+
+def add_app(mun_id, app_id):
+    url = "{0}/{1}/{2}".format(memroy_base_url, app_name, mun_id)
+    req = requests.get(url)
+    if req.status_code == 404:
+        add_municipality(mun_id)
+    req = requests.get(url)
+    if app_id not in req.json():
+        params = {"container_id": app_id}
+        req = requests.post(url, json=params)
+
+
+def add_user(user):
+    url = "{0}/{1}/{2}/{3}".format(
+        memroy_base_url, app_name, user["mun_id"], user["app_id"]
+    )
+    params = user
+    requests.post(url, json=params)
+    # print(f'\tUser {user["user_id"]} added from {user["app_id"]}')
+
+
+def export_to_memory(users):
+    add_app(mun_id, app_id)
+    line_count = 0
+    for user in users:
+        add_user(user)
+        line_count += 1
+        logger.info("{0}/{1} user added".format(line_count, len(users)))
+
+
+if __name__ == "__main__":
     args = parser.parse_args()
-    plone_path = args.plone_path
-    if not plone_path and obj:
-        portal = obj
-    else:
-        portal = get_site(app, plone_path)  # noqa
-    users = get_users(portal)
-    create_csv(users)
+    users = get_users()
+    export_to_memory(users)
